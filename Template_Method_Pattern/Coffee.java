1b2e50cbd760a470560497af6fe339a43cee463f
Govindu Thejana <149306686+Govindu-Thejana@users.noreply.github.com>
2024-08-14 13:46:28 +0530

    Implement Template Method Design Pattern

---
+++ Template_Method_Pattern/Coffee.java
@@ -0,0 +1,9 @@
+public class Coffee extends CaffeineBeverage {
+    public void brew ( ) {
+    System.out.println ("Dripping Coffee Thru the Filters");
+    }
+    public void addCondiments ( ){
+    System.out.println("Adding Sugar and Milk");
+    }
+    }
+    
---
+++ Template_Method_Pattern/Tea.java
@@ -0,0 +1,8 @@
+public class Tea extends CaffeineBeverage {
+    public void brew ( ){
+    System.out.println ("Steeping the Tes");
+    }
+    public void addCondiments ( ) {
+    System.out.println("Adding Lemon");
+    }
+    }
